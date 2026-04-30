209f36cdbfdfb3f3b10ea63bb692b3ed40a754af
telthhealth-cloud <kabilan@mytelth.com>
2026-04-30 16:45:45 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Contact from "./pages/Contact";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
+
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-8B2T4NYFKX", {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -15,6 +30,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <PageTracker/>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<Contact />} /> {/* Add Contact route */}
